c87affda4b050acd993f96d2b1a9428812e6e4cd
lykhachov <ilyalykhachov.com.ua>
2020-12-17 17:38:26 +0200

    renemed db_schema to avoid conflict

--- dev/tools/make_standalone_app.sh
+++ dev/tools/make_standalone_app.sh
@@ -2,5 +2,6 @@
 
 cp composer.json.standalone composer.json
 cp app/etc/storefront_pricing/di.xml.standalone app/etc/storefront_pricing/di.xml
+cp app/etc/db_schema.xml.standalone app/etc/db_schema.xml
 cp app/storefront_pricing_autoload.php app/autoload.php
 cp app/storefront_pricing_bootstrap.php app/bootstrap.php
